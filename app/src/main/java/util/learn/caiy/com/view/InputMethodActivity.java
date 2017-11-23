b99ce1678b39a61de2c75126189a63d8fc43ca93
caiyong <859529943@qq.com>
2017-11-23 16:36:15 +0800

    显示或隐藏输入法

--- app/src/main/java/util/learn/caiy/com/view/InputMethodActivity.java
+++ app/src/main/java/util/learn/caiy/com/view/InputMethodActivity.java
@@ -1,9 +1,15 @@
 package util.learn.caiy.com.view;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import util.learn.caiy.com.util.R;
 
 /**
@@ -14,7 +20,10 @@ import util.learn.caiy.com.util.R;
 
 public class InputMethodActivity extends Activity {
 
+    private Button mSwitchButton;
     private EditText mEditTextView;
+
+    private boolean show = true;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,10 +34,33 @@ public class InputMethodActivity extends Activity {
     }
 
     private void initData() {
+        mSwitchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showInputMethod(show);
+                show = !show;
+            }
+        });
     }
 
     private void initView() {
+        mSwitchButton = (Button)findViewById(R.id.switch_btn);
         mEditTextView = (EditText)findViewById(R.id.edit_tv);
-        
+
+    }
+
+    private void showInputMethod(boolean show) {
+        //自动弹出键盘
+        InputMethodManager inputManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        //切换输入法
+//        inputManager.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
+
+        if (show) {
+            inputManager.showSoftInput(mEditTextView,InputMethodManager.SHOW_FORCED);
+        } else {
+            inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
+        }
+
     }
 }
