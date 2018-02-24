6987fcf6df2c287340bb699551662b2200f3a5f1
caiyong <859529943@qq.com>
2018-02-24 17:43:28 +0800

    修改视频链接地址

--- app/src/main/java/util/learn/caiy/com/activity/VideoCacheActivity.java
+++ app/src/main/java/util/learn/caiy/com/activity/VideoCacheActivity.java
@@ -16,7 +16,8 @@ import util.learn.caiy.com.video.cache.VideoPreloadManager;
 public class VideoCacheActivity extends Activity{
 
 //    private static final String VIDEO_ORIGIN_URL = "https://raw.githubusercontent.com/danikula/AndroidVideoCache/master/files/origin1.mp4";
-    private static final String VIDEO_ORIGIN_URL = "http://jmvideo2.jumei.com/MQ_E_E/MTUxODIyNjYxMTQwMw_E_E/MzEwMjc2MA_E_E/L2hvbWUvd3d3L2xvZ3MvdmlkZW8vZmlsZV84OTkyMTMtNjVjYTg4M2NlMmFmN2NjYTBkZGViNzBmODU1YzIxZDQvdmlkZW8ubXA0_default.mp4";
+    private static final String VIDEO_ORIGIN_URL = "http://27.221.48.205/6571DA84D014B841CCB0B33C2F/03000B03005A5D843223CD048750EBD1FAAC9E-1AA6-D48C-72F5-8F54FBB7BE9F.mp4?ccode=03020101&duration=197&expire=18000&psid=5f97085fb6f51292ba6d89ed50045887&ups_client_netip=70fcc231&ups_ts=1519462404&ups_userid=&utid=yxUYE2woGnYCAXD8wjGCtq1e&vid=XMzMyMjg3MTQzNg%3D%3D&vkey=A15f92de58d9b39f8a654a022a622106c";
+//    private static final String VIDEO_ORIGIN_URL = "http://jmvideo2.jumei.com/MQ_E_E/MTUxODIyNjYxMTQwMw_E_E/MzEwMjc2MA_E_E/L2hvbWUvd3d3L2xvZ3MvdmlkZW8vZmlsZV84OTkyMTMtNjVjYTg4M2NlMmFmN2NjYTBkZGViNzBmODU1YzIxZDQvdmlkZW8ubXA0_default.mp4";
 
     private Button mStartButton;
     private Button mCancelButton;
